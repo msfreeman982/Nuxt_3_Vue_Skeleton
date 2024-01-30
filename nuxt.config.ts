efc910f6fa44d9f83d16493f05b76ebc04519204
Ivan P. <46684023+msfreeman982@users.noreply.github.com>
2024-01-30 22:25:43 +0100

    Add zero config

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+export default {
+    buildModules: ['@nuxtjs/tailwindcss'],
+    tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+        configPath: 'tailwind.config.js',
+        jit: true,
+    },
+}
